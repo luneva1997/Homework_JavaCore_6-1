f6d2bd317c72bfcb060f89a78f7b7fe7cbb8a9ed
luneva1997 <whatswrongnow@mail.ru>
2023-07-01 13:46:30 +0300

    initial_commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,82 @@
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MainTest {
+
+    @Test
+    public void test1(){
+        ByteArrayInputStream in = new ByteArrayInputStream("0".getBytes());
+        Main main = new Main();
+        boolean result = main.Program(in);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void test2(){
+        Main main = new Main();
+        String[] list = {"Задача 1", "Задача 2", "Задача 3"};
+        String answer = "Задача 1\nЗадача 2\nЗадача 3\n";
+        for (String l: list){
+            ByteArrayInputStream in = new ByteArrayInputStream(l.getBytes());
+            main.addWork(in);
+        }
+        String result = "";
+
+        for (String t: main.tasks){
+            result = result + t +"\n";
+        }
+        Assertions.assertEquals(answer, result);
+    }
+
+    @Test
+    public void test3(){
+        Main main = new Main();
+        String[] list = {"Задача 1", "Задача 2", "Задача 3"};
+        String answer = "Список задач:1. Задача 12. Задача 23. Задача 3";
+        for (String l: list){
+            ByteArrayInputStream in = new ByteArrayInputStream(l.getBytes());
+            main.addWork(in);
+        }
+
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        main.printAllWorks();
+        String result = output.toString().replaceAll("\n", "").replaceAll("\r", "");
+
+        Assertions.assertEquals(answer, result);
+    }
+
+    @Test
+    public void test4(){
+        Main main = new Main();
+        String[] list = {"Задача 1", "Задача 2", "Задача 3"};
+        String answer = "Задача 1\nЗадача 2\n";
+        for (String l: list){
+            ByteArrayInputStream in = new ByteArrayInputStream(l.getBytes());
+            main.addWork(in);
+        }
+        ByteArrayInputStream in = new ByteArrayInputStream("3".getBytes());
+        main.deleteWork(in);
+
+        String result = "";
+
+        for (String t: main.tasks){
+            result = result + t +"\n";
+        }
+        Assertions.assertEquals(answer, result);
+
+    }
+
+    @Test
+    public void test5(){
+        ByteArrayInputStream in = new ByteArrayInputStream("dfdfdf".getBytes());
+        Main main = new Main();
+        boolean result = main.Program(in);
+        Assertions.assertTrue(result);
+    }
+}
